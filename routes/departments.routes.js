8b8d21e3ee3ec16b0e97367aee5ec6fc91f3978d
Ola <cencora.aleksandra@gmail.com>
2024-01-09 17:51:21 +0100

    Modify departments enpoints to show modified or deleted element

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -62,7 +62,7 @@ router.put('/departments/:id', async (req, res) => {
     if(dep) { // jeśli szukany departament istnieje
       dep.name = name; // zmień jego atrybut name
       await dep.save(); // zaktualizuj ten dokument
-      res.json({ message: 'OK' });
+      res.json({ message: 'OK', dep });
     }
     else res.status(404).json({ message: 'Not found...' });
   }
@@ -78,7 +78,7 @@ router.delete('/departments/:id', async (req, res) => {
     const dep = await Department.findById(req.params.id);
     if(dep) {
       await Department.deleteOne({ _id: req.params.id });
-      res.json({ message: 'OK' });
+      res.json({ message: 'OK', dep });
     }
     else res.status(404).json({ message: 'Not found...' });
   }
